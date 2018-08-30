b353eca3f57a407f26c2afef11d60b3738edfd25
Steve Andre <andre250584@gmail.com>
2018-08-30 13:01:09 -0500

    se crea Pantalla inicial, pestañas y menu

--- app/src/main/java/com/example/stagetwo/proyectomovilesv1/MainActivity.java
+++ app/src/main/java/com/example/stagetwo/proyectomovilesv1/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.stagetwo.proyectomovilesv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view) {
+        Intent miIntent = new Intent(MainActivity.this,principal.class);
+        startActivity(miIntent);    }
 }
